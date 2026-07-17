852d67b7a707909f2a33fdafe89716fc3b3ac872
Vivek Phad <vivekphad775@gmail.com>
2026-07-17 23:53:59 +0530

    Solution of Q10 - LeetCode Q.1661

---
+++ Q10.sql
@@ -0,0 +1,15 @@
+# Q.1661 AVerage Time of Process per Machine
+
+SELECT
+    machine_id,
+    ROUND(
+        SUM(
+            CASE
+                WHEN activity_type = 'end' THEN timestamp
+                ELSE -timestamp
+            END
+        ) / COUNT(DISTINCT process_id),
+        3
+    ) AS processing_time
+FROM Activity
+GROUP BY machine_id;
